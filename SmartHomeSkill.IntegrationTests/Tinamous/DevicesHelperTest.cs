69b0cc5c3cbe388258c8114e364fa2dc14e46dd8
Stephen Harrison <Stephen.Harrison@AnalysisUK.com>
2018-02-23 08:40:39 +0000

    Added integration tests project and added integrsation tests for tinamous.

---
+++ SmartHomeSkill.IntegrationTests/Tinamous/DevicesHelperTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using Tinamous.SmartHome.Tinamous;
+
+namespace SmartHomeSkill.IntegrationTests.Tinamous
+{
+    [TestFixture]
+    public class DevicesHelperTest
+    {
+        private string _authToken;
+
+        [SetUp]
+        public void Setup()
+        {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .AddJsonFile("secrets.json")
+                .Build();
+
+            _authToken = config["BearerToken"];
+
+            if (string.IsNullOrEmpty(_authToken))
+            {
+                throw new Exception("No auth token");
+            }
+        }
+
+        [Test]
+        public async Task GetDevices()
+        {
+            // Arrange
+            var restClient = new RestClient();
+            DevicesClient helper = new DevicesClient(restClient);
+
+            // Act
+            var devices = await helper.GetDevicesAsync(_authToken);
+
+            // Assert
+            Assert.IsNotNull(devices);
+            Assert.AreEqual(4, devices.Count);
+        }
+    }
+}
---
+++ SmartHomeSkill.IntegrationTests/Tinamous/StatusClientTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using Tinamous.SmartHome.Tinamous;
+
+namespace SmartHomeSkill.IntegrationTests.Tinamous
+{
+    [TestFixture]
+    public class StatusClientTest
+    {
+        private string _authToken;
+
+        [SetUp]
+        public void Setup()
+        {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .AddJsonFile("secrets.json")
+                .Build();
+
+            _authToken = config["BearerToken"];
+
+            if (string.IsNullOrEmpty(_authToken))
+            {
+                throw new Exception("No auth token");
+            }
+        }
+
+        [Test]
+        public async Task PostStatus()
+        {
+            // Arrange
+            var client = new StatusClient(new RestClient());
+
+            // Act
+            await client.PostStatusMessageAsync(_authToken, "Hello from Alexa Unit Tests...");
+
+            // Assert
+            // shouldn't blow up....
+        }
+    }
+}
